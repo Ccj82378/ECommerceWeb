0bd01043ad2173cc3ee442ee355fe03e353ae449
Ccj82378 <jason87328@gmail.com>
2020-10-31 17:30:55 +0800

    update

--- src/component/Product.js
+++ src/component/Product.js
@@ -33,7 +33,7 @@ function Product({ id, title, image, price, rating }) {
                 </div>
             </div>
             <img src={image} alt="" />
-            <button onClick={addToBasket}>Add to Cart</button>
+            <button onClick={addToBasket}>Add to Basket</button>
         </div>
     )
 }
